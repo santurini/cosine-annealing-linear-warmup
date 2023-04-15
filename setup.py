d11b176ea18dc120f6ca3cb45aa52a3e67076a48
Arturo Ghinassi <ghinassiarturo8@gmail.com>
2023-04-15 12:38:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="cosine_linear_warmup",
+    name="cosine_warmup",
     version="0.1",
     author="Arturo Ghinassi",
-    packages=['cosine_linear_warmup'],
+    packages=['cosine_warmup'],
     description="Cosine Annealing Scheduler with Linear Warmup and multiple groups support for PyTorch",
     long_description=open("README.md").read(),
 )
