52cae89a2390a1a218488cca171805359156392e
Arturo Ghinassi <ghinassiarturo8@gmail.com>
2023-04-15 13:30:18 +0200

    Create doctests.py

---
+++ tests/doctests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import doctest
+import cosine_warmup
+import unittest
+
+
+def tests():
+    suite = unittest.TestSuite()
+    modules = [
+        cosine_warmup.scheduler
+    ]
+
+    for m in modules:
+        suite.addTests(doctest.DocTestSuite(m))
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+
+if __name__ == '__main__':
+    tests()
